604465fca31ee0d7240b77cb9fda85b97c5d81f1
Pedro R. Jiménez <countzyx@gmail.com>
2021-02-19 04:03:50 -0500

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogo} alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className={styles.AppLink} href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div></div>;
+};
 
 export default App;
